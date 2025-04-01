0e0fa3d0e2164f701fbb5a475433ba1686928c22
Андрей Солдатов <AFeSoldatov@sberbank.ru>
2025-04-02 00:43:52 +0300

    main - added parse args, predict and train model.

--- predict.py
+++ predict.py
@@ -1,15 +1,23 @@
-from predict_service import PredictService
+from typing import Tuple
+
+from train import LinearRegression as Lr
+
+
+def predict(theta0: float, theta1: float, m: float) -> float:
+    return theta0 + theta1 * m
+
 
 if __name__ == "__main__":
-    predict_instance: PredictService = PredictService()
-    # predict_instance.load_thetas()
-    # mileage: float = float(input("mileage="))
-    # result: float = predict_instance.estimate_price(mileage=mileage)
-    # print(result)
-    # predict_instance.load_dataset()
-    predict_instance.load_dataset()
-    predict_instance.normalize()
-    predict_instance.fit()
-    mileage=float(input())
-    res = predict_instance.estimate_price2(mileage=mileage)
-    print(res)
+    """
+    First program. Predict script.
+    """
+
+    try:
+        mileage: float = float(input("Enter mileage in km:"))
+    except ValueError:
+        raise ValueError("You should send float type!")
+    if mileage < 0:
+        raise ValueError("Mileage can't be negative!")
+    thetas: Tuple[float, float] = Lr.read_thetas()
+    predicted_price: float = predict(theta0=thetas[0], theta1=thetas[1], m=mileage)
+    print(f"Predicted price for {mileage} km: ${predicted_price:.2f}")
